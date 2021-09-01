4dc06cb0fceb18c74204efa3b299518c298ee640
rzkhosroshahi <rzkhosroshahi@gmail.com>
2021-09-01 21:58:41 +0430

    add header component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Header />
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -7,3 +8,13 @@
     <router-view />
   </div>
 </template>
+<script>
+import Header from "@/components/Header/index.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
---
+++ src/components/Header/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <header class="header">
+    <h2 class="header__title">Where in the world?</h2>
+    <div class="header__darkMode" @click="handleDarkMode">
+      <moon class="header__moonIcon" />
+      <p class="header__darkText">Dark Mode</p>
+    </div>
+  </header>
+</template>
+<script>
+import Moon from "./moon";
+
+export default {
+  name: "Header",
+  components: { Moon },
+  data() {
+    return {
+      isDark: false,
+    };
+  },
+  mounted() {
+    const darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    darkQuery.addEventListener("change", (e) => {
+      const currentTheme = e.target.matches ? "dark" : "light";
+      this.applyTheme(currentTheme);
+    });
+  },
+  methods: {
+    handleDarkMode() {
+      this.isDark = !this.isDark;
+
+      if (this.isDark) {
+        document.documentElement.dataset.userColorScheme = "dark";
+        localStorage.setItem("theme", "dark");
+        return;
+      }
+      document.documentElement.dataset.userColorScheme = "light";
+      localStorage.setItem("theme", "light");
+    },
+    getOsTheme() {
+      return getComputedStyle(document.documentElement)
+        .getPropertyValue("--color-mode")
+        .replace(/"/gi, "")
+        .trim();
+    },
+    getStoredTheme() {
+      return window.localStorage.getItem("theme");
+    },
+    applyTheme(theme = "light") {
+      const storedTheme = this.getStoredTheme();
+      const osTheme = this.getOsTheme();
+      const preferredColor = theme || osTheme;
+      const fallBack = storedTheme || preferredColor;
+
+      document.documentElement.dataset.userColorScheme = fallBack;
+      this.isDark = fallBack === "dark";
+    },
+  },
+};
+</script>
+<style lang="scss" src="./style.scss"></style>
