b64abfe3d1468e201b997a2b7fadd13cc5a45ae9
rzkhosroshahi <rzkhosroshahi@gmail.com>
2021-09-02 14:09:08 +0430

    add back button into details page

--- src/views/Details/index.vue
+++ src/views/Details/index.vue
@@ -1,72 +1,80 @@
 <template>
   <p v-if="isLoading">loading...</p>
   <section v-else-if="country" class="country">
-    <div class="country__flag">
-      <img :src="country.flag" class="country__img" rel="preload" alt="" />
+    <div class="country__back">
+      <chips to="/" class="country__backBtn">
+        <icon name="arrow-left" class="country__arrow" />
+        <span>Back</span>
+      </chips>
     </div>
-    <div class="country__info">
-      <h2 class="country__title">
-        {{ country.name }}
-      </h2>
-      <div class="country__specs">
-        <p class="country__details">
-          <span class="country__detailTitle">Native Name</span>:
-          <span class="country__detailSpec">{{ country.nativeName }}</span>
-        </p>
-        <p class="country__details">
-          <span class="country__detailTitle">Population</span>:
-          <span class="country__detailSpec">{{ country.population }}</span>
-        </p>
-        <p class="country__details">
-          <span class="country__detailTitle">Region</span>:
-          <span class="country__detailSpec">{{ country.region }}</span>
-        </p>
-        <p class="country__details">
-          <span class="country__detailTitle">Sub Region</span>:
-          <span class="country__detailSpec">{{ country.subregion }}</span>
-        </p>
-        <p class="country__details">
-          <span class="country__detailTitle">Capital</span>:
-          <span class="country__detailSpec">{{ country.capital }}</span>
-        </p>
-        <p class="country__details">
-          <span class="country__detailTitle">Top Level Domain</span>:
-          <span class="country__detailSpec">
-            {{ country.topLevelDomain.join(",") }}
-          </span>
-        </p>
-        <p class="country__details">
-          <span class="country__detailTitle">currencies</span>:
-          <span
-            class="country__detailSpec"
-            v-for="cr in country.currencies"
-            :key="cr.code"
-          >
-            {{ cr.code }}
-          </span>
-        </p>
-        <p class="country__details">
-          <span class="country__detailTitle">Languages</span>:
-          <span
-            class="country__detailSpec"
-            v-for="ln in country.languages"
-            :key="ln.name"
-          >
-            {{ ln.name }}
-          </span>
-        </p>
+    <div class="country__wrapper">
+      <div class="country__flag">
+        <img :src="country.flag" class="country__img" rel="preload" alt="" />
       </div>
-      <div class="country__borders">
-        <span class="country__borderTitle">Border Countries: </span>
-        <div class="country__borderItems">
-          <chips
-            v-for="border in country.borders"
-            :key="border"
-            class="country__borderItem"
-            sm
-          >
-            {{ border }}
-          </chips>
+      <div class="country__info">
+        <h2 class="country__title">
+          {{ country.name }}
+        </h2>
+        <div class="country__specs">
+          <p class="country__details">
+            <span class="country__detailTitle">Native Name</span>:
+            <span class="country__detailSpec">{{ country.nativeName }}</span>
+          </p>
+          <p class="country__details">
+            <span class="country__detailTitle">Population</span>:
+            <span class="country__detailSpec">{{ country.population }}</span>
+          </p>
+          <p class="country__details">
+            <span class="country__detailTitle">Region</span>:
+            <span class="country__detailSpec">{{ country.region }}</span>
+          </p>
+          <p class="country__details">
+            <span class="country__detailTitle">Sub Region</span>:
+            <span class="country__detailSpec">{{ country.subregion }}</span>
+          </p>
+          <p class="country__details">
+            <span class="country__detailTitle">Capital</span>:
+            <span class="country__detailSpec">{{ country.capital }}</span>
+          </p>
+          <p class="country__details">
+            <span class="country__detailTitle">Top Level Domain</span>:
+            <span class="country__detailSpec">
+              {{ country.topLevelDomain.join(",") }}
+            </span>
+          </p>
+          <p class="country__details">
+            <span class="country__detailTitle">currencies</span>:
+            <span
+              class="country__detailSpec"
+              v-for="cr in country.currencies"
+              :key="cr.code"
+            >
+              {{ cr.code }}
+            </span>
+          </p>
+          <p class="country__details">
+            <span class="country__detailTitle">Languages</span>:
+            <span
+              class="country__detailSpec"
+              v-for="ln in country.languages"
+              :key="ln.name"
+            >
+              {{ ln.name }}
+            </span>
+          </p>
+        </div>
+        <div v-if="country.borders.length" class="country__borders">
+          <span class="country__borderTitle">Border Countries: </span>
+          <div class="country__borderItems">
+            <chips
+              v-for="border in country.borders"
+              :key="border"
+              class="country__borderItem"
+              sm
+            >
+              {{ border }}
+            </chips>
+          </div>
         </div>
       </div>
     </div>
@@ -75,6 +83,7 @@
 <script>
 import { getCountriesByCode } from "./api";
 import Chips from "@/components/chips";
+import Icon from "../../components/Icon";
 
 export default {
   name: "Details",
@@ -85,6 +94,7 @@ export default {
     };
   },
   components: {
+    Icon,
     Chips,
   },
   mounted() {
