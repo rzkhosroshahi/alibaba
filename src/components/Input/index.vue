36ad7b63c396a89884e8f68d40ded3b0410d186d
rzkhosroshahi <rzkhosroshahi@gmail.com>
2021-09-02 10:43:57 +0430

    add input component

---
+++ src/components/Input/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="input">
+    <icon v-if="icon" :name="icon" class="input__icon" />
+    <input
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+      :placeholder="placeholder"
+      class="input__input"
+    />
+    <div class="input__slot">
+      <slot />
+    </div>
+  </div>
+</template>
+<script>
+import Icon from "../Icon";
+
+export default {
+  components: { Icon },
+  props: {
+    value: String,
+    placeholder: String,
+    icon: String,
+  },
+};
+</script>
+<style lang="scss" src="./style.scss"></style>
