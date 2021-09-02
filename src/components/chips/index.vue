a63a0f788b4eb8738de452fa37f88b4cd89bec84
rzkhosroshahi <rzkhosroshahi@gmail.com>
2021-09-02 13:54:11 +0430

    add chip component

---
+++ src/components/chips/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <component :is="cmp" :to="to" :class="className">
+    <slot />
+  </component>
+</template>
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      required: false,
+    },
+    sm: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  computed: {
+    cmp() {
+      return this.to ? "router-link" : "div";
+    },
+    className() {
+      return {
+        chip: true,
+        [`chip--sm`]: this.sm,
+        [`chip--link`]: this.to,
+      };
+    },
+  },
+};
+</script>
+<style lang="scss" src="./style.scss"></style>
